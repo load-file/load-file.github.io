ed51a5b891f6c3b6b4354f6ebf5a72b6411abcca
Danny Engelman <danny@engelman.nl>
2023-01-08 17:42:42 +0100

    minimized cacheJS

--- cached.js
+++ cached.js
@@ -1,32 +1,22 @@
-console.clear();
-customElements.define(
-  "load-file",
-  class extends HTMLElement {
+customElements.define("load-file",class extends HTMLElement {
     async connectedCallback() {
+      let tmpname = this.localName + "-cached";
       let shadowRoot = this.shadowRoot || this.attachShadow({ mode: "open" });
       let src = (this.src = this.getAttribute("src"));
-      shadowRoot.innerHTML = `<load-file-cached src="${src}"></load-file-cached>`;
-      let cache =
-        (this.cache =
-        this.constructor.cache =
-          this.constructor.cache || new Map());
+      shadowRoot.innerHTML = `<${tmpname} src="${src}"></${tmpname}>`;
+      let cache = (this.cache = this.constructor.cache = this.constructor.cache || new Map());
       let content = cache.get(src);
       if (!content) {
         cache.set(src, src);
         cache.set(src, (content = await (await fetch(src)).text()));
-        URIs.append(
-          Object.assign(document.createElement("li"), { innerHTML: src })
-        );
-        if (!customElements.get("load-file-cached")) {
-          customElements.define(
-            "load-file-cached",
-            class extends HTMLElement {
+        URIs?.append(Object.assign(document.createElement("li"), { innerHTML: src }));
+        if (!customElements.get(tmpname)) {
+          customElements.define( tmpname, class extends HTMLElement {
               connectedCallback() {
                 let src = this.getAttribute("src");
                 let host = this.getRootNode().host;
                 let content = host.constructor.cache.get(src);
-                if (!content || content == src)
-                  setTimeout(() => this.connectedCallback(), 0);
+                if (!content || content == src) setTimeout(() => this.connectedCallback(), 0);
                 else host.render(content);
               }
             }
@@ -37,21 +27,7 @@ customElements.define(
     render(content = this.cache.get(this.src)) {
       this.shadowRoot.innerHTML = content;
       this.shadowRoot.append(...this.querySelectorAll("[shadowRoot]"));
-      this.hasAttribute("replaceWith") &&
-        this.replaceWith(...this.shadowRoot.childNodes);
+      this.hasAttribute("replaceWith") && this.replaceWith(...this.shadowRoot.childNodes);
     }
   }
 );
-
-function addSquare(color = "blue") {
-event?.stopImmediatePropagation();
-  let lf = document.createElement("load-file");
-  lf.setAttribute("src", `//svg-cdn.github.io/${color}.svg`);
-  document.body.append(lf);
-}
-setTimeout(() => {
-  addSquare("red");
-  document.body.onclick = () => {
-    addSquare();
-  };
-}, 1000);
